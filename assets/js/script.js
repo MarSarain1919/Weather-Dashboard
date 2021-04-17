a36e2f03d56525fb4c4ae92b16fc913a5a54ef5f
Katja Sarain <katjasarain@gmail.com>
2021-04-17 00:28:07 -0700

    started JS. re-worked previous html to better fit criteria

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const inputEl = document.getElementById('city-input');
+const searchEl = document.getElementById('searchBtn');
+const history = document.getElementById('history');
+const nameEl = document.getElementById('cityName');
+const tempEl = document.getElementById('temp');
+const humidEl = document.getElementById('humidity');
+const windEl = document.getElementById('windSpeed');
+const uvEl = document.getElementById('uV');
+
